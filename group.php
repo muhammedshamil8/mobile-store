02c816a6e6e69583d1aa00366d47349a75adffec
strikerlulu <strikerlulu7@gmail.com>
2023-07-22 21:24:58 +0530

    feat: display errors in webpage

--- group.php
+++ group.php
@@ -1,6 +1,8 @@
 <?php 
 session_start();
-error_reporting(1);
+error_reporting(E_ALL);
+ini_set('display_startup_errors',1);
+ini_set('display_errors',1);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $groupname = $_POST['groupname'];
