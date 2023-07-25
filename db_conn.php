fb3fa29399290984de243fc1d4ae7d3ef737613a
Shamil <muhammedshamil008@gmail.com>
2023-07-26 01:39:55 +0530

    name edit to understand quickly

---
+++ db_conn.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = getenv("MYSQL_HOST") ?: "mysql_db";
+$username = getenv("MYSQL_USER") ?: "root";
+$password = getenv("MYSQL_PASSWORD") ?: "root";
+$database = getenv("MYSQL_DATABASE") ?: "mobile_store"; 
+
+
+try {
+    $pdo = new PDO("mysql:host=$servername;dbname=$database;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
