1fe2cda2702fe8239be591bc4036864f93c3d28f
Shamil <muhammedshamil008@gmail.com>
2023-07-26 02:20:04 +0530

    db export

--- firstWork.session.sql
+++
@@ -1,3 +0,0 @@
-select groupname from `groups`
--- select product_name from device
--- select heading from uploads
--- mobile_store db.sql
+++ mobile_store db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql_db
--- Generation Time: Jul 20, 2023 at 07:03 PM
+-- Generation Time: Jul 25, 2023 at 08:12 PM
 -- Server version: 8.0.33
 -- PHP Version: 8.2.7
 
@@ -38,7 +38,24 @@ CREATE TABLE `device` (
 --
 
 INSERT INTO `device` (`id`, `product_name`, `groupid`) VALUES
-(1, 'shamil', 1);
+(1, 'shamil', 1),
+(2, 'lulu', 2),
+(3, 'ronaldo', 3),
+(4, 'l', 1),
+(5, 'luluuuuu', 1),
+(6, 'messi', 3),
+(8, 'dd', 10),
+(12, 'first product', 14),
+(14, 'chat', 12),
+(15, 'first product', 15),
+(16, 'lp', 10),
+(17, 'shamil', 20),
+(18, 'mm', 16),
+(19, 'lkl', 16),
+(20, 'hh', 16),
+(21, 'mk', 16),
+(22, 'kk', 16),
+(23, 'Jj', 21);
 
 -- --------------------------------------------------------
 
@@ -48,15 +65,23 @@ INSERT INTO `device` (`id`, `product_name`, `groupid`) VALUES
 
 CREATE TABLE `groups` (
   `id` int NOT NULL,
-  `groupname` varchar(255) NOT NULL
+  `groupname` varchar(255) NOT NULL,
+  `userid` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `groups`
 --
 
-INSERT INTO `groups` (`id`, `groupname`) VALUES
-(1, 'Mom');
+INSERT INTO `groups` (`id`, `groupname`, `userid`) VALUES
+(10, 'yes', 10),
+(12, 'd', 10),
+(14, 'lulu', 10),
+(15, 'harifa', 7),
+(16, 'mm', 7),
+(20, 'shamil', 10),
+(21, 'group', 10),
+(22, 'jj', 7);
 
 -- --------------------------------------------------------
 
@@ -66,7 +91,7 @@ INSERT INTO `groups` (`id`, `groupname`) VALUES
 
 CREATE TABLE `upload` (
   `product_id` int NOT NULL,
-  `image` varchar(500) NOT NULL,
+  `image` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `heading` varchar(255) NOT NULL,
   `description` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -76,7 +101,30 @@ CREATE TABLE `upload` (
 --
 
 INSERT INTO `upload` (`product_id`, `image`, `heading`, `description`) VALUES
-(1, 'C.jpeg', 'happiness', 'it  was something feelings in heart can\'t describe from words');
+(1, 'C.jpeg', 'happiness', 'it  was something feelings in heart can\'t describe from words..!'),
+(2, 'text.jpg', 'journey', 'Life is like a journey '),
+(3, 'helo.jpg', 'fly ', 'we want to fly !\r\n'),
+(12, 'torism.jpeg', 'ff', 'dd');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(7, 'ashii', 'yes'),
+(10, 'demo', 'demoo');
 
 --
 -- Indexes for dumped tables
@@ -92,13 +140,20 @@ ALTER TABLE `device`
 -- Indexes for table `groups`
 --
 ALTER TABLE `groups`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_userid` (`userid`);
 
 --
 -- Indexes for table `upload`
 --
 ALTER TABLE `upload`
   ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -108,19 +163,35 @@ ALTER TABLE `upload`
 -- AUTO_INCREMENT for table `device`
 --
 ALTER TABLE `device`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `upload`
 --
 ALTER TABLE `upload`
-  MODIFY `product_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `product_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `groups`
+--
+ALTER TABLE `groups`
+  ADD CONSTRAINT `fk_userid` FOREIGN KEY (`userid`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
