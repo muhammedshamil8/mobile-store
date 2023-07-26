ba9ae185c7355ff042c4c0ba9262628245090519
strikerlulu <strikerlulu7@gmail.com>
2023-07-26 23:55:43 +0530

    fix: db connection

--- db_conn.php
+++ db_conn.php
@@ -1,6 +1,6 @@
 <?php
 
-$servername = getenv("MYSQL_HOST") ?: "mysql_db";
+$servername = getenv("MYSQL_HOST") ?: "mysql";
 $username = getenv("MYSQL_USER") ?: "root";
 $password = getenv("MYSQL_PASSWORD") ?: "root";
 $database = getenv("MYSQL_DATABASE") ?: "mobile-store"; 
